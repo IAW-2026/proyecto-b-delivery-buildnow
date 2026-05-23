bb4fd7e5b22e48791ece899cc69ab45d17bdba65
Martina <martumoreno6@gmail.com>
2026-05-23 14:17:54 -0300

    style: se añadieron componentes de botón y selección con estilos globales. - Se añadieron componentes atómicos de selección y botón para reutilizar los elementos de la interfaz. - Se actualizaron las configuraciones CSS globales. - Se actualizaron las dependencias del proyecto.

--- delivery-app/src/app/layout.tsx
+++ delivery-app/src/app/layout.tsx
@@ -2,10 +2,15 @@
 import type { PropsWithChildren } from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="es">
+    <html lang="es" className={cn("font-sans", geist.variable)}>
       <body>
         <ClerkProvider>{children}</ClerkProvider>
       </body>
