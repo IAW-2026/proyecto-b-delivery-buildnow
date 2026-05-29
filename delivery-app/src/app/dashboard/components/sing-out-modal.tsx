a749460002337f8fe3931e937a38d65009370376
Martina <martumoreno6@gmail.com>
2026-05-29 19:48:11 -0300

    feat: implementar panel de control visual para el administrador
    - Crear vista principal y layout en /dashboard/admin.
    - Diseñar componentes para métricas generales y tabla interactiva de repartidores.
    - Añadir formulario interactivo para editar repartidores y tipos de vehículo.

--- delivery-app/src/app/dashboard/components/sing-out-modal.tsx
+++ delivery-app/src/app/dashboard/components/sing-out-modal.tsx
@@ -30,7 +30,7 @@ export function SignOutDialog() {
         </DialogHeader>
 
         <div className="mt-4 flex justify-end gap-2">
-          <DialogClose>
+          <DialogClose asChild>
             <button className="rounded-lg border px-4 py-2 cursor-pointer hover:bg-gray-100 transition-colors">
               Cancelar
             </button>
